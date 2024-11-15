b6ede35829915f49a03e9604f4d60e1134449c58
amir-awad <amirawad004@gmail.com>
2024-11-15 16:35:17 +0200

    fix: load scenes using menue buttons works well

--- Assets/gameStory.cs
+++ Assets/gameStory.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.Playables;
 using UnityEngine.UI; // Added for button
+using UnityEngine.SceneManagement;
 
 public class gameStory : MonoBehaviour
 {
-    public GameObject leftButton;
-    public GameObject rightButton;
+    public Button leftButton;
+    public Button rightButton;
     public GameObject choicePanel;
     public GameObject loadTimelineButton;
     public TextMeshProUGUI myText;
     private PlayableDirector playableDirector;
 
+
     void Start()
     {
-        //if (leftButton != null)
-        //{
-        //    leftButton.onClick.AddListener(() => { SceneManager.LoadScene(leftSceneName); });
-        //}
+        if (leftButton != null)
+        {
+           leftButton.onClick.AddListener(() => { SceneManager.LoadScene("Amir"); });       
+        }
+
+        if (rightButton != null)
+        {
+            rightButton.onClick.AddListener(() => { SceneManager.LoadScene("Ibrahim"); });
+        }
 
         choicePanel.SetActive(false);
         playableDirector = GetComponent<PlayableDirector>();
@@ -45,7 +52,7 @@ public class gameStory : MonoBehaviour
 
     public void PlayTimeline()
     {
-        myText.text = "Hello… can you hear me? Is anyone out there?\r\n\r\nYour ship, the Stellar Horizon, was on a routine mission when disaster struck—a glowing asteroid collided with the hull, forcing a crash landing on an uncharted planet. You awaken surrounded by alien landscapes, the air thick with unfamiliar sounds and dangers lurking in the shadows. The wreckage of your ship is scattered, and your crew is missing. With limited resources, your mission is clear: explore the planet to gather supplies, uncover its secrets, and locate your crew. The path back to safety won’t be easy—hostile creatures and ancient mysteries stand in your way. Every decision you make could mean survival or being lost forever on this alien world.";
+        myText.text = "Helloï¿½ can you hear me? Is anyone out there?\r\n\r\nYour ship, the Stellar Horizon, was on a routine mission when disaster struckï¿½a glowing asteroid collided with the hull, forcing a crash landing on an uncharted planet. You awaken surrounded by alien landscapes, the air thick with unfamiliar sounds and dangers lurking in the shadows. The wreckage of your ship is scattered, and your crew is missing. With limited resources, your mission is clear: explore the planet to gather supplies, uncover its secrets, and locate your crew. The path back to safety wonï¿½t be easyï¿½hostile creatures and ancient mysteries stand in your way. Every decision you make could mean survival or being lost forever on this alien world.";
 
         playableDirector.Play(); // Play the timeline
         loadTimelineButton.SetActive(false); // Disable the button
